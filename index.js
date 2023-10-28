48d3f7db6c11af5ce3a209cdc72cb02998a20b3c
jchaveza17 <jesuschavez1804@gmail.com>
2023-10-27 19:38:57 -0700

    function to display cards with name and css update

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('whats up gangy gangy');
+document.addEventListener('DOMContentLoaded', function () {
+  const pokemonList = document.querySelector('.cardContainer');
+
+  async function getPokemonNames() {
+    try {
+      const response = await axios.get(
+        'https://pokeapi.co/api/v2/generation/1/'
+      );
+      const data = response.data;
+      const pokemonNames = data.pokemon_species;
+
+      pokemonNames.forEach((pokemon) => {
+        const pokeList = document.createElement('li');
+        pokeList.textContent = pokemon.name;
+        pokemonList.appendChild(pokeList);
+      });
+    } catch (error) {
+      console.error('Error fetching Pokémon data:', error);
+    }
+  }
+  getPokemonNames();
+});
