b9b0bf912f86fa14849f3724754a2412a7aa2773
jchaveza17 <jesuschavez1804@gmail.com>
2023-11-08 18:44:51 -0700

    search eventlistener added.

--- index.js
+++ index.js
@@ -3,8 +3,25 @@ async function getPokemonData() {
     const response = await axios.get('https://pokeapi.co/api/v2/generation/1/');
     const pokemonData = response.data;
     const cardContainer = document.getElementById('pokemonContainer');
+    const searchInput = document.getElementById('searchPokemon');
 
-    for (pokemonEntry of pokemonData.pokemon_species) {
+    searchInput.addEventListener('input', () => {
+      const search = searchInput.value.toLowerCase();
+
+      for (const listItem of cardContainer.getElementsByTagName('li')) {
+        const pokemonName = listItem.textContent
+          .split('Name: ')[1]
+          .split(' ')[0]
+          .toLowerCase();
+        if (pokemonName.includes(search)) {
+          listItem.style.display = 'block';
+        } else {
+          listItem.style.display = 'none';
+        }
+      }
+    });
+
+    for (const pokemonEntry of pokemonData.pokemon_species) {
       const pokemon = pokemonEntry.name;
 
       const pokemonType = await axios.get(
@@ -24,4 +41,5 @@ async function getPokemonData() {
     console.error('An error occurred:', error);
   }
 }
+
 getPokemonData();
